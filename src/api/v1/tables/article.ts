e604498a92cc77724b30fd1c76ebe21a81e01b1f
Max <max.luetkemeyer@outlook.de>
2024-11-20 14:43:51 +0000

    Refactor article routes to handle errors and improve response validation; add ordering for article retrieval

--- src/api/v1/tables/article.ts
+++ src/api/v1/tables/article.ts
@@ -4,64 +4,118 @@ import { PrismaClient } from "@prisma/client";
 const router = express.Router({ mergeParams: true });
 const prisma = new PrismaClient();
 
-router.get("/", async (req, res) => {
-	const articles = await prisma.article.findMany();
+// READ All
+router.get("/", async (_, res) => {
+	const articles = await prisma.article
+		.findMany({
+			orderBy: {
+				article_id: "asc",
+			},
+		})
+		.catch((e) => {
+			console.error(e);
+		});
+
+	if (articles == null) {
+		res.status(400).json({ message: "Invalid request" });
+		return;
+	}
 
 	res.json(articles);
 });
 
+// READ specific
 router.get("/:id", async (req, res) => {
-	const article = await prisma.article.findUnique({
-		where: {
-			article_id: parseInt(req.params.id),
-		},
-	});
+	const article = await prisma.article
+		.findUnique({
+			where: {
+				article_id: parseInt(req.params.id),
+			},
+		})
+		.catch((e) => {
+			console.error(e);
+		});
 
 	if (article == null) {
-		res.status(404).json({ message: "Article not found" });
+		res.status(400).json({ message: "Invalid request" });
 		return;
 	}
 
 	res.json(article);
 });
 
+// CREATE
 router.post("/", async (req, res) => {
-	const article = await prisma.article.create({
-		data: {
-			title: req.body.title,
-			content: req.body.content,
-			created_at: new Date(),
-			user_id: req.body.user_id, //TODO: CHECK
-		},
-	});
+	const article = await prisma.article
+		.create({
+			data: {
+				title: req.body.title,
+				content: req.body.content,
+				created_at: new Date(),
+				user_id: req.body.user_id,
+			},
+		})
+		.catch((e) => {
+			console.error(e);
+		});
+
+	if (article == null) {
+		res.status(400).json({ message: "Invalid request" });
+		return;
+	}
 
 	res.json(article);
 });
 
-//TODO: use old or new if present
+// UPDATE
 router.put("/:id", async (req, res) => {
-	console.log("put gemacht", req.body);
-	const article = await prisma.article.update({
-		where: {
-			article_id: parseInt(req.params.id),
-		},
-		data: {
-			title: req.body.title,
-			content: req.body.content,
-			user_id: Number(req.body.user_id),
-			updated_at: new Date(),
-		},
-	});
-	
+	const id = parseInt(req.params.id);
+
+	const article = await prisma.article
+		.update({
+			where: {
+				article_id: id,
+			},
+			data: {
+				title: req.body.title,
+				content: req.body.content,
+				user_id: req.body.user_id
+					? Number(req.body.user_id)
+					: undefined,
+				updated_at: new Date(),
+				created_at: req.body.created_at
+					? new Date(req.body.created_at)
+					: undefined,
+			},
+		})
+		.catch((e) => {
+			console.error(e);
+		});
+
+	if (article == null) {
+		res.status(400).json({ message: "Invalid request" });
+		return;
+	}
+
 	res.json(article);
 });
 
+// DELETE
 router.delete("/:id", async (req, res) => {
-	const article = await prisma.article.delete({
-		where: {
-			article_id: parseInt(req.params.id),
-		},
-	});
+	const article = await prisma.article
+		.delete({
+			where: {
+				article_id: parseInt(req.params.id),
+			},
+		})
+		.catch((e) => {
+			console.error(e);
+		});
+
+	if (article == null) {
+		res.status(400).json({ message: "Invalid request" });
+		return;
+	}
 
 	res.json(article);
 });
