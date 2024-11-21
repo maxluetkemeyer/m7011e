17bd0f60c4160471e7bef3eb91e538b1173fbb7d
Max <max.luetkemeyer@outlook.de>
2024-11-21 15:03:30 +0000

    Enhance user management interface: fetch and display users with TOTP status and groups; update navigation links

--- src/routes/dashboard/admin/routes.ts
+++ src/routes/dashboard/admin/routes.ts
@@ -4,8 +4,21 @@ import { PrismaClient } from "@prisma/client";
 const router = express.Router({ mergeParams: true });
 const prisma = new PrismaClient();
 
-router.get("/edit_users", (_, res) => {
-	res.render("users/admin/edit_users", { myVar: "Hey" });
+router.get("/edit_users", async (_, res) => {
+	const users = await prisma.users.findMany({
+		orderBy: {
+			user_id: "asc",
+		},
+		include: {
+			user_group_member: {
+				include: {
+					user_group: true,
+				},
+			}
+		},
+	});
+
+	res.render("users/admin/edit_users", { users });
 });
 
 router.get("/edit_user", (_, res) => {
--- src/routes/dashboard/routes.ts
+++ src/routes/dashboard/routes.ts
@@ -4,12 +4,14 @@ import { groupAuthorization } from "../../authorization.js";
 import { MyJWT } from "../../jwt.js";
 import adminRouter from "./admin/routes.js";
 import totpRouter from "./totp/routes.js";
+import articleRouter from "./article/routes.js";
 
 const router = express.Router({ mergeParams: true });
 const prisma = new PrismaClient();
 
 router.use(groupAuthorization(["admin"]), adminRouter);
 router.use(totpRouter);
+router.use(articleRouter);
 
 router.get("/", async (_, res) => {
 	const articles = await prisma.article.findMany({
