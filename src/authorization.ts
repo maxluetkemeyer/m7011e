6c3c726ba0b91a6aba64dd054e4b2d9c91b27d87
Max <max.luetkemeyer@outlook.de>
2024-11-21 22:31:52 +0000

    Refresh JWT on page visit; Enhance JWT handling: add JWT signing functionality, refresh token on payload read, and remove unused example and Google Cloud files

--- src/authorization.ts
+++ src/authorization.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import { verifyJWT } from "./jwt.js";
+import { signJWT, verifyJWT } from "./jwt.js";
 
 export function groupAuthorization(allowedGroups: string[]) {
 	return async (req: Request, res: Response, next: NextFunction) => {
@@ -43,12 +43,27 @@ export function readJWTPayload() {
 		}
 		const verifyResult = await verifyJWT(jwt);
 
+		// If the JWT is invalid, continue without setting the payload
 		if (typeof verifyResult === "string") {
 			next();
 			return;
 		}
 
 		res.locals.my_jwtPayload = verifyResult;
+
+		// Refresh JWT
+		const jwt_refreshed = await signJWT({
+			user_id: verifyResult.user_id,
+			name: verifyResult.name,
+			email: verifyResult.email,
+			groups: verifyResult.groups,
+		});
+		res.cookie("jwt", jwt_refreshed, {
+			httpOnly: true,
+			sameSite: "strict",
+			secure: true,
+			maxAge: 1000 * 60 * 15,
+		}); // 15 minutes
 
 		next();
 	};
