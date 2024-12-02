22abecc55662656030e1afa2392915888995ada6
Max <max.luetkemeyer@outlook.de>
2024-12-02 18:30:13 +0000

    port name

--- auth/main.ts
+++ auth/main.ts
@@ -6,12 +6,12 @@ const app = (await import("./app.js")).app;
 const appInternal = (await import("./app_internal.js")).appInternal;
 
 const AUTH_PORT = process.env.AUTH_PORT;
-const AUTH_PORT_INTERNAL = process.env.AUTH_PORT_INTERNAL;
+const AUTH_SERVICE_PORT = process.env.AUTH_SERVICE_PORT;
 
 app.listen(AUTH_PORT, () => {
 	console.log(`[AUTH] Listening on ${AUTH_PORT}`);
 });
 
-appInternal.listen(AUTH_PORT_INTERNAL, () => {
-	console.log(`[AUTH INTERNAL] Listening on ${AUTH_PORT_INTERNAL}`);
+appInternal.listen(AUTH_SERVICE_PORT, () => {
+	console.log(`[AUTH INTERNAL] Listening on ${AUTH_SERVICE_PORT}`);
 });
