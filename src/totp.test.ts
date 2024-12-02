c6165681943916f32a3eeb3ed55d3b31a07df55d
Max <max.luetkemeyer@outlook.de>
2024-12-02 21:59:49 +0000

    tests

--- src/totp.test.ts
+++ src/totp.test.ts
@@ -1,12 +1,11 @@
 import { describe, expect, test } from "@jest/globals";
 // @ts-ignore
-import { generateTOTPSecret } from "./totp";
+import { totp_to_url, generateTOTPSecret } from "./totp";
 
 describe("validateToken", () => {
-	test("should return true when token is valid", () => {
-		
-        
-	});
+	test("happy path", () => {});
+
+	test("unhappy path", () => {});
 });
 
 describe("generateTOTPSecret", () => {
@@ -24,16 +23,70 @@ describe("generateTOTPSecret", () => {
 		expect(first).not.toBe(second);
 	});
 
-    test("two generated secrets with different sizes should not be of equal length", () => {
-        const first: string = generateTOTPSecret({ size: 10 });
-        const second: string = generateTOTPSecret({ size: 20 });
+	test("two generated secrets with different sizes should not be of equal length", () => {
+		const first: string = generateTOTPSecret({ size: 10 });
+		const second: string = generateTOTPSecret({ size: 20 });
 
-        expect(first.length).not.toBe(second.length);
-    });
+		expect(first.length).not.toBe(second.length);
+	});
 });
 
 describe("totp_to_url", () => {
 	test("should return a string", () => {
 		expect(typeof "string").toBe("string");
+	});
+
+	test("check params", () => {
+		const secret: string = generateTOTPSecret();
+
+		const got: string = totp_to_url(secret, "my_account");
+		console.log(got);
+
+		const parts = got.split("?");
+		const base = parts[0];
+		const paramString = parts[1];
+		const paramList = paramString.split("&");
+		const params: any = {};
+
+		for (const param of paramList) {
+			const parts = param.split("=");
+			const key = parts[0];
+			const value = parts[1];
+
+			params[key] = value;
+		}
+
+		console.log(params);
+
+		const checkIssuer = params.issuer === "Lulea%20Newspaper";
+		const checkSecret = params.secret === secret.toString();
+		const checkAlgorithm = params.algorithm === "SHA1";
+		const checkDigits = params.digits === "6";
+		const checkPeriod = params.period === "30";
+
+		expect(checkIssuer).toBeTruthy();
+		expect(checkSecret).toBeTruthy();
+		expect(checkAlgorithm).toBeTruthy();
+		expect(checkDigits).toBeTruthy();
+		expect(checkPeriod).toBeTruthy();
+	});
+
+	test("check base", () => {
+		const secret: string = generateTOTPSecret();
+
+		const got: string = totp_to_url(secret, "my_account");
+		console.log(got);
+
+		const parts = got.split("?");
+		const base = parts[0];
+		
+		console.log(base)
+
+		const checkStart = base.startsWith("otpauth://totp/Lulea%20Newspaper:")
+		const checkAccount = base.endsWith("my_account")
+
+		expect(checkStart).toBeTruthy();
+		expect(checkAccount).toBeTruthy();
+
 	});
 });
